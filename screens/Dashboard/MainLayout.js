d1ec363db54b99822fc7d31cd8a956ddbfecec3e
cuong.phamviet <cuong.phamviet@euroland.com>
2022-03-18 17:24:17 +0700

    update

--- screens/Dashboard/MainLayout.js
+++ screens/Dashboard/MainLayout.js
@@ -1,11 +1,65 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Animated,
+  FlatList,
+} from 'react-native';
+import {Home, Profile, Search} from '../../screens';
+import {COLORS, SIZES, FONTS, constants} from '../../constants';
 
 const MainLayout = () => {
+  const flatListRef = React.useRef();
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  const renderContent = () => {
+    return (
+      <View style={{flex: 1}}>
+        <Animated.FlatList
+          ref={flatListRef}
+          horizontal
+          pagingEnabled
+          snapToAlignment="center"
+          snapToInterval={SIZES.width}
+          decelerationRate="fast"
+          //   showsHorizontalScrollIndicator={false}
+          data={constants.bottom_tabs}
+          keyExtractor={item => `Main -${item.id}`}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: scrollX,
+                  },
+                },
+              },
+            ],
+            {
+              useNativeDriver: false,
+            },
+          )}
+          renderItem={(item, index) => {
+            return (
+              <View style={{height: SIZES.height, width: SIZES.width}}>
+                {item.label == constants.screens.home && <Home />}
+                {item.label == constants.screens.search && <Search />}
+                {item.label == constants.screens.profile && <Profile />}
+              </View>
+            );
+          }}
+        />
+        {/* <Text>hiii</Text> */}
+      </View>
+    );
+  };
+
   return (
-    <SafeAreaView>
-      <Text>MainLayout</Text>
-    </SafeAreaView>
+    <View style={{flex: 1, backgroundColor: COLORS.white}}>
+      {renderContent()}
+    </View>
   );
 };
 
